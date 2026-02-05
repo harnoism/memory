51746f605e3fe8662ab70cd5698436d5cc4ffc51
harnoism <harnois.maevaa@gmail.com>
2026-02-05 19:12:08 +0100

    Javascript terminé

--- script.js
+++ script.js
@@ -23,3 +23,64 @@ const gameGrid = shuffleArray(cardsArray.concat(cardsArray)); //Création de la 
 function shuffleArray(array) {
     return array.sort(() => 0.5 - Math.random());
 }
+
+function createCard(item){
+    const card=document.createElement('div');
+    card.classList.add=item.name;
+
+    const front =document.createElement('div');
+    black.style.backgroundImage =`url(${item.img})`;
+
+    card.appendChild(front);
+    card.appendChild(black);
+
+    return card
+}
+
+function initGameBoard(){ //Parcourons les cartes mélangées et ajoutons chq au conteneur du jeu
+    gameGrid.forEach(item => {
+        const card = createCard(item);
+        game.appendChild(card);
+    });
+}
+
+function handleCardClick(event){  //Add un écouteur d'event pr les clics sur les cartes
+    const clicked = event.target;
+
+    if(count<2) {
+        count++;
+        clicked.parentNode.classList.add('selected','flipped');
+        if (count===1){
+            firstGuess=clicked.parentNode.dataset.name;
+        } else {
+            secondGuess=clicked.parentNode.dataset.name;
+
+            if (firstGuess && secondGuess){
+                if(firstGuess===secondGuess){
+                    setTimeout(match,delay);
+                }
+                setTimeout(resetGuesses,delay);
+            }
+        }
+        previousTarget=clicked
+    }
+}
+
+//===réinitialisons les tentatives===
+function match(){
+    const selected= document.querySelectorAll('.selected');
+    selected.forEach(card => card.classList.add('match'));
+}
+
+function resetGuesses(){
+    firstGuess='';
+    secondGuess='';
+    count=0;
+    previousTarget=null;
+
+    const selected=document.querySelectorAll('.selected');
+    selected.forEach(card => card.classList.remove('selected','flipped'));
+}
+
+game.addEventListener('click', handleCardClick);
+initGameBoard();
